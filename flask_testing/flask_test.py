6a22630d425cdb5430b7f625876e621ecbc5cb56
Will Spence <20wds@queensu.ca>
2025-02-03 13:33:10 -0500

    Fixed the API key

--- flask_testing/flask_test.py
+++ flask_testing/flask_test.py
@@ -1,22 +1,19 @@
-# No existing code
 from flask import Flask, request, jsonify
 import cohere
 from system_prompts.what_prompt_v4 import what_prompt
 from data_cleaning import clean_text
 
+with open('cohere_api_key.txt', 'r') as file:
+    COHERE_API_KEY = file.read().strip()
 
 app = Flask(__name__)
 
-cohere_client = cohere.Client('85pa1ijYVknf5xRslpLJ5lB0sBCUXmDyyEPSoQKn')
+co = cohere.ClientV2(COHERE_API_KEY)
 
 @app.route('/post_content', methods=['POST'])
 def post_content():
     data = request.json
     page_content = data.get('pageContent', '')
-    # print('Received content:', page_content)
-
-
-    co = cohere.ClientV2("85pa1ijYVknf5xRslpLJ5lB0sBCUXmDyyEPSoQKn")
     response = co.chat(
         model="command-r-plus", 
         messages=[{"role": "system", "content": what_prompt},
